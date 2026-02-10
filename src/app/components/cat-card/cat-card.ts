418477a099dcda4f35c57b2b240e368262e2c801
alerdd <g.igumentsev968@gmail.com>
2026-02-10 22:11:59 +0500

    Начало проекта: Cat Gallery на Angular

---
+++ src/app/components/cat-card/cat-card.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cat-card',
+  imports: [],
+  templateUrl: './cat-card.html',
+  styleUrl: './cat-card.scss',
+})
+export class CatCard {
+
+}
